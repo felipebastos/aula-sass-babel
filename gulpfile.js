b229fdf286b84a04f562c57c203ce6a1a86025ed
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2021-02-19 16:51:54 -0300

    Agora convertendo imagens

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
 const sass = require('gulp-dart-sass');
+const webp = require('gulp-webp');
 
 function limpar(){
-    return src(['build/js/*.min.js', 'build/css/*.css'])
+    return src(['build/js/*.min.js', 'build/css/*.css', 'build/img/*.webp'])
     .pipe(clean());
 }
 
@@ -19,17 +20,24 @@ function js(){
 }
 
 exports.default = function(){
-    watch('src/js/*.js', series(limpar, js));
+    watch('src/css/*.sass', css);
+    watch('src/js/*.js', js);
 }
 
 function css(){
     return src('src/css/*.scss')
     .pipe(sass())
     .pipe(rename({extname: '.min.css'}))
-    .pipe(dest('build/css'))
+    .pipe(dest('src/css'))
 }
 
+function images(){
+    return src('src/img/*.jpg')
+    .pipe(webp())
+    .pipe(dest('build/img'));
+}
 
+exports.buildwebp = images;
 exports.buildjs = series(limpar, js);
 exports.clean = limpar;
 exports.buildcss = css;
